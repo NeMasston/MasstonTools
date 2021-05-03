63ce7996cb21c1df5afa9ea480601f37a2390a5b
MSTNXYZ <mr.bearka2005@gmail.com>
2021-05-03 12:01:10 +0300

    Начало проекта

---
+++ src/main/java/me/mstn/MasstonTools/Main.java
@@ -0,0 +1,15 @@
+package me.mstn.MasstonTools;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
